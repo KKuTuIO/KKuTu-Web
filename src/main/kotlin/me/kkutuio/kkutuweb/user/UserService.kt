46c983ff65d3f73bc0a1eb049b848e3eb79331e3
Preta-Crowz <preta@siro.dev>
2022-10-07 17:40:44 +0900

    냩장식 변경 + 손 관련 문제 해결

--- src/main/kotlin/me/kkutuio/kkutuweb/user/UserService.kt
+++ src/main/kotlin/me/kkutuio/kkutuweb/user/UserService.kt
@@ -91,7 +91,7 @@ class UserService(
         else if (part == "Mhand") {
             part = if (isLeft) "Mlhand" else "Mrhand"
             val equipingGood = user.box.get(id)
-            val isUnequip = user.equip.get(part).toString() == id
+            val isUnequip = user.equip.has(part) && user.equip.get(part).toString() == id
             if (isUnequip) {
                 // 장착 해제
             } else {

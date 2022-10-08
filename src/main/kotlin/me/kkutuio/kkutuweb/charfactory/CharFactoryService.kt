4ecd42e49c81864c5744ac30b063eca7c9eb7f1a
Do Hoon Kim <op@kdh.io>
2022-10-08 11:37:39 -1000

    Update CharFactoryService.kt

--- src/main/kotlin/me/kkutuio/kkutuweb/charfactory/CharFactoryService.kt
+++ src/main/kotlin/me/kkutuio/kkutuweb/charfactory/CharFactoryService.kt
@@ -226,9 +226,9 @@ class CharFactoryService(
 
         val rewards = ArrayList<Reward>()
         rewards.add(Reward("dictPage", wordLength * 1.0))
-        rewards.add(Reward("boxE4", wordLength * 0.375, true))
+        rewards.add(Reward("boxE2", wordLength * 0.375, true))
         if (wordLength > 1) rewards.add(Reward("boxE3", (wordLength - 1) * 0.25, true))
-        if (wordLength > 2) rewards.add(Reward("boxE2", (wordLength - 2) * 0.175, true))
+        if (wordLength > 2) rewards.add(Reward("boxE4", (wordLength - 2) * 0.175, true))
         rewards.add(Reward("\$WPE?", wordLength * 0.25, true))
 
         return CFResult(cost, rewards)

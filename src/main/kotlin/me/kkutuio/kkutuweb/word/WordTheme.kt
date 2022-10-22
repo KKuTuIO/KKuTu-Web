d45320d4864a0bd2e3b3701eb0c3dae348ba4d75
Do Hoon Kim <op@kdh.io>
2022-10-21 23:04:00 -1000

    Update WordTheme.kt

--- src/main/kotlin/me/kkutuio/kkutuweb/word/WordTheme.kt
+++ src/main/kotlin/me/kkutuio/kkutuweb/word/WordTheme.kt
@@ -163,7 +163,7 @@ enum class WordTheme(val themeCode: String, val themeName: String) {
     THEME_520("520", "해양"),
     THEME_530("530", "화학"),
     THEME_1001("1001", "나라 이름과 수도"),
-    THEME_PKT("KKT", "쿵쿵따"),
+    THEME_PKT("KKT", "쿵쿵따 전용 단어"),
     THEME_HBW("HBW", "어인정 한방 방어 단어"),
     THEME_SBW("SBW", "안전 입력 제한 단어");
 

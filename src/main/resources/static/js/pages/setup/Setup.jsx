5c55889a009492b5710d7dd742d92b577070b943
Dohoon Kim <op@kdh.io>
2024-04-23 20:49:02 -1000

    Update Setup.jsx

--- src/main/resources/static/js/pages/setup/Setup.jsx
+++ src/main/resources/static/js/pages/setup/Setup.jsx
@@ -55,9 +55,9 @@ const Setup = () => {
         <>
             <SetupTitle>끄투리오에서 사용하실 별명을 입력해 주세요.</SetupTitle>
 
-            <HelpTitle>별명 규칙</HelpTitle>
+            <HelpTitle>별명 설정 안내</HelpTitle>
             <HelpText>1. 별명은 2 ~ 15글자의 영문자, 한글, 숫자, 공백, 특수문자 -, _로 설정하실 수 있습니다.</HelpText>
-            <HelpText>2. 별명은 7일마다 변경하실 수 있으며, 500핑을 사용하여 고정할 수 있습니다.</HelpText>
+            <HelpText>2. 별명은 7일마다 변경하실 수 있으며, 250핑을 사용하여 고정할 수 있습니다.</HelpText>
             <HelpText>3. 욕설, 비속어 등이 포함된 별명을 사용할 경우 운영정책에 따라 제재될 수 있습니다.</HelpText>
 
             <NickInput placeholder={Messages['kkutu.dialog.dress.nickname.placeHolder']} name="nickname"

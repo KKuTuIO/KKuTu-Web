1481a76e704343073f237654b3fa26501560d099
Gyubin Lee <support@pcor.me>
2025-03-08 01:42:16 +0900

    test

--- Frontpage/src/routes/+layout.svelte
+++ Frontpage/src/routes/+layout.svelte
@@ -1,32 +1,38 @@
-<script nonce="kkutuio">
+<script>
 	import { page } from '$app/stores';
 	import Header from './Header.svelte';
 	import HeaderEvent from './Header_Event.svelte';
 	import HeaderMan from './Header_Maintenance.svelte';
 	import HeaderOkggg from './Header_Okggg.svelte';
 	import Footer from './Footer.svelte';
+	import Dialog from './Dialog.svelte';
+	import { showDialog } from './dialogStore';
+  
 	import './styles.css';
 	import '../app.css';
-
+  
 	const okggg = ["rank", "okggg", "stats"];
-</script>
-
-<div class="app">
-	{#if $page.url.pathname.includes('man')}
-	<HeaderMan/>
-	{:else if okggg.some((v) => $page.url.pathname.includes(v))}
-	<HeaderOkggg/>
-	{:else if !$page.url.pathname.includes('event_')}
-	<Header />
-	{:else}
-	<HeaderEvent />
-	{/if}
-
-	<main>
-		<slot />
-	</main>
-
-	{#if !$page.url.pathname.includes('event_')}
-	<Footer />
-	{/if}
-</div>
+  </script>
+  
+  <div class="app">
+	  {#if $page.url.pathname.includes('man')}
+	  <HeaderMan />
+	  {:else if okggg.some((v) => $page.url.pathname.includes(v))}
+	  <HeaderOkggg />
+	  {:else if !$page.url.pathname.includes('event_')}
+	  <Header />
+	  {:else}
+	  <HeaderEvent />
+	  {/if}
+  
+	  <main>
+		  <slot />
+	  </main>
+  
+	  {#if !$page.url.pathname.includes('event_')}
+	  <Footer />
+	  {/if}
+  
+	  <Dialog />
+  </div>
+  

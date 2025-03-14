92962ad5cd1cf5b11f4ad432d179364f031f0022
Gyubin Lee <support@pcor.me>
2025-03-15 03:23:45 +0900

    Update

--- src/main/kotlin/me/kkutuio/kkutuweb/MainController.kt
+++ src/main/kotlin/me/kkutuio/kkutuweb/MainController.kt
@@ -146,9 +146,10 @@ class MainController(
             val images = listOf(
                 "https://cdn.kkutu.io/img/kkutu/intro/tips/0.png",
                 //"https://cdn.kkutu.io/img/kkutu/intro/tips/1.png",
-                "https://cdn.kkutu.io/img/kkutu/intro/tips/2.png",
+                //"https://cdn.kkutu.io/img/kkutu/intro/tips/2.png",
                 //"https://cdn.kkutu.io/img/kkutu/intro.png"
-                "https://cdn.kkutu.io/img/kkutu/intro/spring.png"
+                "https://cdn.kkutu.io/img/kkutu/intro/spring.png",
+                "https://cdn.kkutu.io/img/kkutu/intro/event/darae_activation.png"
             )
     
             val randomImage = images.random()

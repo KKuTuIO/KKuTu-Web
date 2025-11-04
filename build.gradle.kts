75f1bd3d91779d46a78cf9799102121d77e4d59c
Dohoon Kim <op@kdh.io>
2025-11-04 02:15:21 -0500

    Update Version Code

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 }
 
 group = "me.kkutuio"
-version = "v3"
+version = "v4"
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = (env, options) => {
         },
         output: {
             path: deployPath,
-            filename: '[name]-v4.0.5c.min.js'
+            filename: '[name]-v4.0.5d.min.js'
         },
         module: {
             rules: [

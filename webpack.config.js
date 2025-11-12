3dee4daea7957f7224ca2ca3e396e0cf279c6323
Dohoon Kim <op@kdh.io>
2025-11-12 15:01:09 -0500

    Update Version Name

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = (env, options) => {
         },
         output: {
             path: deployPath,
-            filename: '[name]-v4.1.0a.min.js'
+            filename: '[name]-v4.1.0b.min.js'
         },
         module: {
             rules: [

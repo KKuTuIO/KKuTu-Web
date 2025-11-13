6bb62e220e2db8168b1ccbc59c51f07d0004d49e
Dohoon Kim <op@kdh.io>
2025-11-13 18:00:36 -0500

    Bump JS asset version to 4.1.1a

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = (env, options) => {
         },
         output: {
             path: deployPath,
-            filename: '[name]-v4.1.0e.min.js'
+            filename: '[name]-v4.1.1a.min.js'
         },
         module: {
             rules: [

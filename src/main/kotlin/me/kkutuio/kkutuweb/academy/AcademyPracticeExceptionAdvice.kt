933d0794df9d53afd2d3d29ed44de5c06359fd5b
Dohoon Kim <op@kdh.io>
2026-08-26 22:51:24 +0900

    Add project license header to academy advice

--- src/main/kotlin/me/kkutuio/kkutuweb/academy/AcademyPracticeExceptionAdvice.kt
+++ src/main/kotlin/me/kkutuio/kkutuweb/academy/AcademyPracticeExceptionAdvice.kt
@@ -1,3 +1,12 @@
+/*
+ * KKuTu-Web (https://github.com/KKuTuIO/KKuTu-Web)
+ * Copyright (C) 2021 KKuTuIO <admin@kkutu.io>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 package me.kkutuio.kkutuweb.academy
 
 import jakarta.servlet.http.HttpServletResponse
